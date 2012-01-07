847e1c7a91821b7930996fd10d9288bbcb389abe
Ulf Hermann <ulf_hermann@gmx.net>
2012-01-07 15:40:58 +0100

    -Feature: record capacities of links

--- src/saveload/saveload.cpp
+++ src/saveload/saveload.cpp
@@ -245,7 +245,7 @@
  *  179   24810
  *  180   24998
  */
-extern const uint16 SAVEGAME_VERSION = SL_RESERVATION; ///< Current savegame version of OpenTTD.
+extern const uint16 SAVEGAME_VERSION = SL_CAPACITIES; ///< Current savegame version of OpenTTD.
 
 SavegameType _savegame_type; ///< type of savegame we are loading
 
