667e46f24885f85cc5125a810b53da5e391dddc2
Ulf Hermann <ulf_hermann@gmx.net>
2012-01-07 16:35:06 +0100

    -Feature: consider cargo waiting at other stations for rating at the origin station

--- src/saveload/saveload.cpp
+++ src/saveload/saveload.cpp
@@ -245,7 +245,7 @@
  *  179   24810
  *  180   24998
  */
-extern const uint16 SAVEGAME_VERSION = SL_CARGOMAP; ///< Current savegame version of OpenTTD.
+extern const uint16 SAVEGAME_VERSION = SL_EXT_RATING; ///< Current savegame version of OpenTTD.
 
 SavegameType _savegame_type; ///< type of savegame we are loading
 
