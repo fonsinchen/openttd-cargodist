a371010e617bf158befeb008d43c28b0275bb161
Ulf Hermann <ulf_hermann@gmx.net>
2013-01-20 17:33:58 +0100

    -Codechange: Introduce save/load version naming

--- src/saveload/saveload.cpp
+++ src/saveload/saveload.cpp
@@ -245,7 +245,7 @@
  *  179   24810
  *  180   24998
  */
-extern const uint16 SAVEGAME_VERSION = 180; ///< Current savegame version of OpenTTD.
+extern const uint16 SAVEGAME_VERSION = SL_TRUNK; ///< Current savegame version of OpenTTD.
 
 SavegameType _savegame_type; ///< type of savegame we are loading
 
